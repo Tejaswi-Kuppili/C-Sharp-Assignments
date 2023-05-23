2e4a026a0e2fff681501fed8061e2ec8a2be17ef
Tejaswi-Kuppili <134361254+Tejaswi-Kuppili@users.noreply.github.com>
2023-05-24 00:01:35 +0530

    Update Program.cs

--- SubString/SubString/TestCasesSubString/TestCasesSubString/Program.cs
+++ SubString/SubString/TestCasesSubString/TestCasesSubString/Program.cs
@@ -1,21 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 
 class SubString
 {
     public static void Main(string[] args)
     {
-        string s1 = "abcdabcabd";
-        string s2 = "ab";
         string s1 = Console.ReadLine();
         string s2 = Console.ReadLine();
         Console.WriteLine(s1.ToUpper());
         Console.WriteLine(s2.ToUpper());
-        int occured = 0;
+        int occurance = 0;
         int index = s1.IndexOf(s2);
         Console.WriteLine("INDEX POSITIONS");
         while (index >= 0)
@@ -24,7 +17,7 @@ class SubString
             occured++;
             index = s1.IndexOf(s2, index + 1);
         }
-        Console.WriteLine("\nNUMBER OF OCCURANCES: " + occured);
+        Console.WriteLine("\nNUMBER OF OCCURANCES: " + occurance);
         Console.ReadKey();
     }
 }
