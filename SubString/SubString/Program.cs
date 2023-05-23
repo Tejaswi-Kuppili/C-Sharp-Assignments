f3b4c88d94985993dff344001562d38702e8cd0e
Tejaswi-Kuppili <134361254+Tejaswi-Kuppili@users.noreply.github.com>
2023-05-24 00:10:47 +0530

    Program.cs

--- SubString/SubString/Program.cs
+++ SubString/SubString/Program.cs
@@ -1,25 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-
- class SubString
+using System;
+class SubString
 {
     public static void Main(string[] args)
     {
         string s1 = Console.ReadLine();
         string s2 = Console.ReadLine();
-        Console.WriteLine(s1.ToUpper());
-        Console.WriteLine(s2.ToUpper());
-        int occured = 0;
+        s1 = s1.ToUpper();
+        s2 = s2.ToUpper();
+        int occurance = 0;
         int index = s1.IndexOf(s2);
         Console.WriteLine("INDEX POSITIONS");
         while (index >= 0)
         {
             Console.WriteLine(index + " ");
-            occured++;
+            occurances++;
             index = s1.IndexOf(s2, index + 1);
         }
         Console.WriteLine("\nNUMBER OF OCCURANCES: " + occured);
