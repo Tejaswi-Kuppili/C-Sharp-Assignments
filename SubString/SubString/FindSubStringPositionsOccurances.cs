dfa920bf5f1aed178ed94766592200d3d81cd317
Tejaswi-Kuppili <134361254+Tejaswi-Kuppili@users.noreply.github.com>
2023-05-24 12:40:24 +0530

    FindSubStringPositionsOccurances.cs

--- SubString/SubString/FindSubStringPositionsOccurances.cs
+++ SubString/SubString/FindSubStringPositionsOccurances.cs
@@ -1,7 +1,8 @@
-﻿class Solution
+class Solution
 {
     public int noOfOccurances = 0;
     public int index;
+    //Method to find substring is present or not
     public bool FindSubString(string s1, string s2)
     {
         int index = s1.IndexOf(s2);
@@ -14,6 +15,7 @@
             return true;
         }
     }
+    //Method to find Index Positions and Number of Occurances
     public int PositionsOccurances(string str1, string str2)
     {
         Console.WriteLine("Index Positions:");
