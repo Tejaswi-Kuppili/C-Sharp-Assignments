240712fed23d087fc848b3612cc18fae5fa2d0f2
Tejaswi-Kuppili <134361254+Tejaswi-Kuppili@users.noreply.github.com>
2023-05-24 12:21:56 +0530

    Add files via upload

---
+++ GreatestProduct/GreatestProduct/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string num = Console.ReadLine();
+        AdjacentDigitProduct product = new AdjacentDigitProduct();
+        Console.WriteLine("Max Product: " + product.GreatestProduct(num));
+        Console.ReadLine();
+    }
+}
