702582d417642a006752d1446f312e75dc488725
Tejaswi-Kuppili <134361254+Tejaswi-Kuppili@users.noreply.github.com>
2023-05-24 11:54:37 +0530

    Add files via upload

---
+++ SubString/SubString/FindSubStringPositionsOccurances.cs
@@ -0,0 +1,29 @@
+﻿class Solution
+{
+    public int noOfOccurances = 0;
+    public int index;
+    public bool FindSubString(string s1, string s2)
+    {
+        int index = s1.IndexOf(s2);
+        if (index == -1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    public int PositionsOccurances(string str1, string str2)
+    {
+        Console.WriteLine("Index Positions:");
+
+        while (index >= 0)
+        {
+            Console.WriteLine(index + " ");
+            noOfOccurances++;
+            index = str1.IndexOf(str2, index + 1);
+        }
+        return noOfOccurances;
+    }
+}
